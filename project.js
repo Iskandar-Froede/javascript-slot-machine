339bd6370f047c5d0cb92da1992632e7b3cd373e
Lucyane Iskandar-Fröde <iskandar.lucyane@gmail.com>
2023-04-03 20:20:43 +0200

    step1-2

--- project.js
+++ project.js
@@ -6,12 +6,54 @@
 // 6. give the user their winnings
 // 7. play again
 
+// step 1
+
 // call the package prompt-sync
 const prompt = require("prompt-sync") ();
 
 // function deposit
 const deposit = () => {
+
+    //loop with condition true = forever looping
+    while (true) {
     const depositAmount = prompt("Enter a deposit amount: ")
+
+// parseFloat => convert string to float number
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <=0) {
+        console.log("Invalid deposit amount, try again.");
+    } else {
+        return numberDepositAmount;
+    }
+    }
 }
 
-deposit();
+// step 2
+
+const getNumberOfLines = () => {
+     //loop with condition true = forever looping
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ")
+    
+    // parseFloat => convert string to float number
+        const numberOfLines = parseFloat(lines);
+    
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+        }
+}
+
+// step 3
+
+const collectBetAmount = () => {
+    
+}
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
+
+ 
