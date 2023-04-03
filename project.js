c21dd9c771abecb60906d3ecd099341107dceede
Lucyane Iskandar-Fröde <iskandar.lucyane@gmail.com>
2023-04-03 20:04:54 +0200

    first commit

---
+++ project.js
@@ -0,0 +1,17 @@
+// 1. deposit some money
+// 2. determine number of line to bet on
+// 3. collect a bet amount
+// 4. spin the slot machine
+// 5. check if the user won
+// 6. give the user their winnings
+// 7. play again
+
+// call the package prompt-sync
+const prompt = require("prompt-sync") ();
+
+// function deposit
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ")
+}
+
+deposit();
