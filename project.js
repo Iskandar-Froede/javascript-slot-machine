4412a5350183b1d8812002c36ff2afe5cde7729b
Lucyane Iskandar-Fröde <iskandar.lucyane@gmail.com>
2023-04-04 15:25:31 +0200

    step 4

--- project.js
+++ project.js
@@ -97,20 +97,56 @@ const spin = () => {
         }
 
     }
-    const reels = [[], [], []];
+    const reels = [];
     for (let i = 0; i < COLS; i++) {
+        reels.push([]);
+        const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
-            
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
         }
     }
+    return reels;
 }
 
-spin();
 
+
+ const transpose = (reels) => {
+    const rows = [],
+
+    for (let i = 0;  i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i])
+        }
+
+    }
+
+    return rows
+ }
+
+ const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i, symbol] of rows.entries()) {
+            rowString += symbol
+            if (i != row.length - 1) {
+                rowString += " | "
+            }
+        }
+        console.log(rowString)
+    }
+ }
 // use let because the balance changes everytime user plays
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
+
 
 
  
